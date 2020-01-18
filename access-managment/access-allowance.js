7a8d53770958348ad0203f24cf88aa09eed1e931
userq11 <unlimited5511@interia.pl>
2020-01-18 23:07:30 +0100

    fix

--- access-managment/access-allowance.js
+++ access-managment/access-allowance.js
@@ -1,11 +1,22 @@
-const { queryDB } = require("./../standaloneCon");
+const { queryDB2 } = require("./../standaloneCon");
 const roleFile = require("./role.json");
+// const responseFile = require("./response.json");
 
 class AccessAllowance {
   constructor() {
     this.role = roleFile.role;
     this.permissions = "";
-    this.asdf = queryDB("select * from roles");
+    this.query = "select * from roles";
+    this.asdf = queryDB2(this.query);
+  }
+
+  // setResponseByQuery(query) {
+  //   queryDB(query);
+  //   this.asdf = responseFile;
+  // }
+
+  setQuery(query) {
+    this.query = query;
   }
 
   addChild() {
@@ -19,10 +30,25 @@ class AccessAllowance {
   getChild() {
     return undefined;
   }
+
+  async doAsdf() {}
 }
 
 const accessAllowance = new AccessAllowance();
 
 console.log(accessAllowance.role);
+
+const doAsync = async () => {
+  const allowanceLevel1 = await accessAllowance.asdf;
+  accessAllowance.setQuery("select * from acl");
+  const allowanceLevel2 = await accessAllowance.asdf;
+
+  console.log(allowanceLevel1);
+  console.log(allowanceLevel2);
+};
+
+doAsync();
+
+// console.log(accessAllowance.asdf);
 
 // console.log(conn);
